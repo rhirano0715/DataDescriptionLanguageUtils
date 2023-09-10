0dfae8772b4d4339ce39819ff003023a30000fec
rhirano0715 <hirano.ryuko.0715@gmail.com>
2023-09-11 03:41:32 +0900

    add test

--- python/src/ddlu.py
+++ python/src/ddlu.py
@@ -10,12 +10,16 @@ def main() -> None:
 
     validate(input_file_path, output_file_path)
 
+    run(input_file_path, output_file_path)
+
+def run(input_file_path, output_file_path):
     data = read(input_file_path)
     write(output_file_path, data)
 
 def validate(input_file_path: pathlib.Path, output_file_path: pathlib.Path):
     if not input_file_path.is_file():
         raise ValueError("Input file is not exists.")
+
     if not output_file_path.parent.is_dir():
         raise ValueError("Output directory is not exists.")
 
@@ -23,6 +27,7 @@ def read(input_file_path: pathlib.Path):
     if input_file_path.suffix == '.json':
         with input_file_path.open(mode="r") as inf:
             return json.load(inf)
+
     if input_file_path.suffix == '.xml':
         return xmltodict.parse(input_file_path.read_text())
 
@@ -33,7 +38,10 @@ def write(output_file_path:pathlib.Path, data):
         with output_file_path.open(mode="w") as otf:
             otf.write(json.dumps(data, indent=4))
             return
+
     if output_file_path.suffix == '.xml':
+        if type(data) is list:
+            data = {"items": {"item": data}}
         with output_file_path.open(mode="w") as otf:
             otf.write(xmltodict.unparse(data, pretty=True))
             return
--- python/tests/Test_ddlu.py
+++ python/tests/Test_ddlu.py
@@ -1,7 +1,83 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-import src.ddlu
+import pathlib
+import src.ddlu as ddlu
 
-def test_dummy():
-    assert 1 == 1, "hello"
+TESTDATA_ROOT = pathlib.Path(__file__).parent.parent / "testdata"
+INPUT_DIR = TESTDATA_ROOT / "input"
+INPUT_JSON = INPUT_DIR / "test.json"
+INPUT_XML = INPUT_DIR / "test.xml"
+OUTPUT_DIR = TESTDATA_ROOT / "output"
+
+def test_json_to_json():
+    excepted = """[
+    {
+        "Key1": "Value1",
+        "Key2": "Value2",
+        "Key3": "Value3",
+        "Key4": "Value4"
+    },
+    {
+        "Key1": "Value1",
+        "Key2": "Value2",
+        "Key3": "Value3",
+        "Key4": "Value4"
+    }
+]"""
+
+    actual = OUTPUT_DIR / "fromjson.json"
+    ddlu.run(INPUT_JSON, actual)
+
+    assert excepted == actual.read_text()
+
+def test_json_to_xml():
+    excepted = """<?xml version="1.0" encoding="utf-8"?>
+<items>
+	<item>
+		<Key1>Value1</Key1>
+		<Key2>Value2</Key2>
+		<Key3>Value3</Key3>
+		<Key4>Value4</Key4>
+	</item>
+	<item>
+		<Key1>Value1</Key1>
+		<Key2>Value2</Key2>
+		<Key3>Value3</Key3>
+		<Key4>Value4</Key4>
+	</item>
+</items>"""
+
+    actual = OUTPUT_DIR / "fromjson.xml"
+    ddlu.run(INPUT_JSON, actual)
+
+    assert excepted == actual.read_text()
+
+def test_xml_to_json():
+    excepted = """{
+    "note": {
+        "to": "Tove",
+        "from": "Jani",
+        "heading": "Reminder",
+        "body": "Don't forget me this weekend!"
+    }
+}"""
+
+    actual = OUTPUT_DIR / "fromxml.json"
+    ddlu.run(INPUT_XML, actual)
+
+    assert excepted == actual.read_text()
+
+def test_xml_to_xml():
+    excepted = """<?xml version="1.0" encoding="utf-8"?>
+<note>
+	<to>Tove</to>
+	<from>Jani</from>
+	<heading>Reminder</heading>
+	<body>Don't forget me this weekend!</body>
+</note>"""
+
+    actual = OUTPUT_DIR / "fromxml.xml"
+    ddlu.run(INPUT_XML, actual)
+
+    assert excepted == actual.read_text()
