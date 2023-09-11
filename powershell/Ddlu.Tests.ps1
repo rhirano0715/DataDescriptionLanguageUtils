38e9918ede1d18fa1fea6cf6ba926dcb488bfebc
rhirano0715 <hirano.ryuko.0715@gmail.com>
2023-09-11 13:46:43 +0900

    create

---
+++ powershell/Ddlu.Tests.ps1
@@ -0,0 +1,110 @@
+$HERE = Split-Path -Parent $MyInvocation.MyCommand.Path
+$SUT = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$HERE\$SUT" "dummy InputFilePath" "dummy OutputFilePath"
+
+$TestdataRoot = Split-Path -Parent $HERE | Join-Path -ChildPath "testdata"
+$InputRoot = Join-Path $TestdataRoot "input"
+$InputJson = Join-Path $InputRoot "test.json"
+$InputXml = Join-Path $InputRoot "test.xml"
+$OutputRoot = Join-Path $TestdataRoot "output"
+$OutputFromJsonJson = Join-Path $OutputRoot "fromjson.json"
+$OutputFromJsonXml = Join-Path $OutputRoot "fromjson.xml"
+$OutputFromXmlJson = Join-Path $OutputRoot "fromxml.json"
+$OutputFromXmlXml = Join-Path $OutputRoot "fromxml.xml"
+
+Describe "DdluMain test" {
+    It "Json2Json" {
+        $expected = @(
+            '[',
+            '    {',
+            '        "Key1":  "Value1",',
+            '        "Key2":  "Value2",',
+            '        "Key3":  "Value3",',
+            '        "Key4":  "Value4"',
+            '    },',
+            '    {',
+            '        "Key1":  "Value1",',
+            '        "Key2":  "Value2",',
+            '        "Key3":  "Value3",',
+            '        "Key4":  "Value4"',
+            '    }',
+            ']'
+        )
+
+        DdluMain $InputJson $OutputFromJsonJson
+        $actual = Get-Content $OutputFromJsonJson
+        $actual | Should  Be $expected
+    }
+    It "Json2Xml not surported" {
+        $expected = @(
+            '[',
+            '    {',
+            '        "Key1":  "Value1",',
+            '        "Key2":  "Value2",',
+            '        "Key3":  "Value3",',
+            '        "Key4":  "Value4"',
+            '    },',
+            '    {',
+            '        "Key1":  "Value1",',
+            '        "Key2":  "Value2",',
+            '        "Key3":  "Value3",',
+            '        "Key4":  "Value4"',
+            '    }',
+            ']'
+        )
+
+        DdluMain $InputJson $OutputFromJsonXml
+        $actual = Get-Content $OutputFromJsonXml
+        $actual | Should  Be $expected
+    }
+    It "Xml2Json" {
+        $expected = @(
+            '{',
+            '    "notes":  {',
+            '                  "note":  [',
+            '                               {',
+            '                                   "to":  "Tove",',
+            '                                   "from":  "Jani",',
+            '                                   "heading":  "Reminder",',
+            '                                   "body":  "Don\u0027t forget me this weekend!"',
+            '                               },',
+            '                               {',
+            '                                   "to":  "to",',
+            '                                   "from":  "from",',
+            '                                   "heading":  "head",',
+            '                                   "body":  "body"',
+            '                               }',
+            '                           ]',
+            '              }',
+            '}',
+            ']'
+        )
+
+        DdluMain $InputXml $OutputFromXmlJson
+        $actual = Get-Content $OutputFromXmlJson
+        $actual | Should  Be $expected
+    }
+    It "Xml2Xml" {
+        $expected = @(
+            "<?xml version=`"1.0`" encoding=`"utf-8`"?>",
+            "<notes>",
+            "    <note>",
+            "        <to>Tove</to>",
+            "        <from>Jani</from>",
+            "        <heading>Reminder</heading>",
+            "        <body>Don't forget me this weekend!</body>",
+            "    </note>",
+            "    <note>",
+            "        <to>to</to>",
+            "        <from>from</from>",
+            "        <heading>head</heading>",
+            "        <body>body</body>",
+            "    </note>",
+            "</notes>"
+        )
+
+        DdluMain $InputXml $OutputFromXmlXml
+        $actual = Get-Content $OutputFromXmlXml
+        $actual | Should  Be $expected
+    }
+}
---
+++ powershell/Ddlu.ps1
@@ -0,0 +1,191 @@
+Param(
+    [Parameter(Mandatory = $true, HelpMessage = "Input file path")]
+    [string]$InputFilePath,
+    [Parameter(Mandatory = $true, HelpMessage = "Output file path")]
+    [string]$OutputFilePath
+)
+
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = "Stop"
+
+$UTF8 = [System.Text.Encoding]::GetEncoding("utf-8")
+
+function DdluMain {
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = "Input file path")]
+        [string]$InputFilePath,
+        [Parameter(Mandatory = $true, HelpMessage = "Output file path")]
+        [string]$OutputFilePath
+    )
+    
+    try {
+        Validate $InputFilePath $OutputFilePath
+        $private:data = ReadFile $InputFilePath
+        WriteFile $OutputFilePath  $private:data
+    }
+    catch {
+        Write-Error ($_.Exception.Message + " " + $PSItem.ScriptStackTrace)
+    }
+}
+
+function Validate {
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = "Input file path")]
+        [string]$InputFilePath,
+        [Parameter(Mandatory = $true, HelpMessage = "Output file path")]
+        [string]$OutputFilePath
+    )
+
+    if (![System.IO.File]::Exists($InputFilePath)) {
+        Write-Error "Input file is not exists."
+    }
+
+    $private:outputDirectoryPath = Split-Path -Parent $OutputFilePath
+    if (![System.IO.Directory]::Exists($private:outputDirectoryPath)) {
+        Write-Error "Output directory is not exists."
+    }
+}
+
+function ReadFile {
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = "Input file path")]
+        [string]$InputFilePath
+    )
+
+    $private:extension = [System.IO.Path]::GetExtension($InputFilePath)
+    if ($private:extension -eq ".json") {
+        $private:data = Get-Content $InputFilePath | ConvertFrom-Json
+        return $private:data
+    }
+    if ($private:extension -eq ".xml") {
+        $private:data = [xml](Get-Content $InputFilePath)
+        $private:convedData = Convert-XmlToHashTable $private:data
+        return $private:convedData
+    }
+
+    Write-Error "Reading $private:extension is not supported."
+}
+
+function Convert-XmlToHashTable {
+    param (
+        [Parameter(Mandatory = $true)]
+        [System.Xml.XmlNode] $XmlNode
+    )
+
+    $hashTable = [ordered] @{}
+
+    foreach ($attribute in $XmlNode.Attributes) {
+        $hashTable[$attribute.Name] = $attribute.Value
+    }
+
+    foreach ($childNode in $XmlNode.ChildNodes) {
+        if ($childNode.NodeType -eq 'Element') {
+            $childData = Convert-XmlToHashTable -XmlNode $childNode
+
+            if ($childData.Count -eq 1 -and $childData.Contains('#text')) {
+                $childData = $childData['#text']
+            }
+
+            if ($hashTable.Contains($childNode.Name)) {
+                if ($hashTable[$childNode.Name] -isnot [Array]) {
+                    $hashTable[$childNode.Name] = @($hashTable[$childNode.Name])
+                }
+                $hashTable[$childNode.Name] += $childData
+            }
+            else {
+                $hashTable[$childNode.Name] = $childData
+            }
+        }
+        elseif ($childNode.NodeType -eq 'Text') {
+            $hashTable['#text'] = $childNode.Value
+        }
+    }
+
+    return $hashTable
+}
+
+function WriteFile {
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = "Output file path")]
+        [string]$OutputFilePath,
+        [Parameter(Mandatory = $true, HelpMessage = "Output data")]
+        $OutputData
+    )
+
+    $private:extension = [System.IO.Path]::GetExtension($OutputFilePath)
+    if ($private:extension -eq ".json") {
+        $private:json = $OutputData | ConvertTo-Json -Depth 100
+        $private:json | Set-Content $OutputFilePath
+        return
+    }
+    if ($private:extension -eq ".xml") {
+        $private:xml = New-Object System.Xml.XmlDocument
+
+        ConvertTo-Xml -InputObject $OutputData -XmlDocument $private:xml -ParentNode $private:xml
+
+        $xmlwriter = New-Object System.Xml.XmlTextWriter($OutputFilePath, $UTF8)
+        $xmlwriter.Formatting = [System.Xml.Formatting]::Indented
+        $xmlwriter.IndentChar = " "
+        $xmlwriter.Indentation = 4
+        $private:xml.Save($xmlwriter)
+        $xmlwriter.Close()
+        return
+    }
+
+    Write-Error "Writing $private:extension is not supported."
+}
+
+function ConvertTo-Xml {
+    param (
+        [Parameter(Mandatory = $true)]
+        $InputObject,
+        [Parameter(Mandatory = $true)]
+        [System.Xml.XmlDocument]$XmlDocument,
+        [Parameter(Mandatory = $false)]
+        $ParentNode
+    )
+
+    if ($InputObject -is [System.Collections.Specialized.OrderedDictionary]) {
+        foreach ($entry in $InputObject.GetEnumerator()) {
+            ProcessEntry -Entry $entry -XmlDocument $XmlDocument -ParentNode $ParentNode
+        }
+    }
+    else {
+        foreach ($property in $InputObject.PSObject.Properties) {
+            ProcessEntry -Entry $property -XmlDocument $XmlDocument -ParentNode $ParentNode
+        }
+    }
+}
+
+function ProcessEntry {
+    param (
+        [Parameter(Mandatory = $true)]
+        $Entry,
+        [Parameter(Mandatory = $true)]
+        [System.Xml.XmlDocument]$XmlDocument,
+        [Parameter(Mandatory = $true)]
+        $ParentNode
+    )
+
+    if ($Entry.Value -is [Hashtable] -or $Entry.Value -is [PSCustomObject] -or $Entry.Value -is [System.Collections.Specialized.OrderedDictionary]) {
+        $element = $XmlDocument.CreateElement($Entry.Name)
+        ConvertTo-Xml -InputObject $Entry.Value -XmlDocument $XmlDocument -ParentNode $element
+        $null = $ParentNode.AppendChild($element)
+    }
+    elseif ($Entry.Value -is [Array]) {
+        foreach ($item in $Entry.Value) {
+            $subElement = $XmlDocument.CreateElement($Entry.Name)
+            ConvertTo-Xml -InputObject $item -XmlDocument $XmlDocument -ParentNode $subElement
+            $null = $ParentNode.AppendChild($subElement)
+        }
+    }
+    else {
+        $element = $XmlDocument.CreateElement($Entry.Name)
+        $element.InnerText = $Entry.Value
+        $null = $ParentNode.AppendChild($element)
+    }
+}
+
+If ((Resolve-Path -Path $MyInvocation.InvocationName).ProviderPath -eq $MyInvocation.MyCommand.Path) {
+    DdluMain $InputFilePath $OutputFilePath
+}
